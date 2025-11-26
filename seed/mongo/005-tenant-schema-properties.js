f9de9cf6b79c2f20bf9c94fd11497df365c711f6
Johan List <johan.list@di.huc.knaw.nl>
2025-11-26 16:37:33 +0100

    Addition of new collection 'schema-properties' for storing type info

---
+++ seed/mongo/005-tenant-schema-properties.js
@@ -0,0 +1,68 @@
+const TENANT_DB = "tenant-a";
+const DATASET_NAME = "politieke-tijdschriften";
+
+const tenantDb = db.getSiblingDB(TENANT_DB);
+
+tenantDb.schema_properties.deleteMany({ dataset_name: DATASET_NAME });
+tenantDb.schema_properties.insertMany([
+    {
+        dataset_name: DATASET_NAME,
+        property: 'titel',
+        order: 0,
+        schema: { "type": "string", "format": "uri", "x-ui-widget": "link" }
+    },
+    {
+        dataset_name: DATASET_NAME,
+        property: 'korteOmschrijvingInhoud',
+        order: 0,
+        schema: { "type": "string", "x-ui-widget": "prose" }
+    },
+    {
+        dataset_name: DATASET_NAME,
+        property: 'uitgever',
+        order: 0,
+        schema: { "type": "string", "format": "uitgever" }
+    },
+     {
+        dataset_name: DATASET_NAME,
+        property: 'plaats',
+        order: 0,
+        schema: { "type": "string", "format": "plaats" }
+    },
+    {
+        dataset_name: DATASET_NAME,
+        property: 'aanvullendeTitels',
+        order: 0,
+        schema: { "type": "array" }
+    },
+    {
+        dataset_name: DATASET_NAME,
+        property: 'vormTijdschrift',
+        order: 0,
+        schema: { "type": "string", "format": "vorm-tijdschrift"}
+    },
+     {
+        dataset_name: DATASET_NAME,
+        property: 'typeTijdschrift',
+        order: 0,
+        schema: { "type": "string", "format": "type-tijdschrift" }
+    },
+     {
+        dataset_name: DATASET_NAME,
+        property: 'politiekePositie',
+        order: 0,
+        schema: { "type": "string", "format": "politieke-positie" }
+    },
+    {
+        dataset_name: DATASET_NAME,
+        property: 'verantwoordingSelectie',
+        order: 0,
+        schema: { "type": "string", "format": "verantwoording-selectie" }
+    },
+    {
+        dataset_name: DATASET_NAME,
+        property: 'toelichtingRedacteurAuteur',
+        order: 0,
+        schema: { "type": "string", "format": "toelichting-redacteur-auteur" }
+    }
+]);
